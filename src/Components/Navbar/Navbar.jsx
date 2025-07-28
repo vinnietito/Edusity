3d6bfe3386148449c4ff4e5cfdc1212d5faa6e7b
vinnietito <vincentnjuguna69@gmail.com>
2025-07-28 20:01:17 +0300

    Commits

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,6 +7,14 @@ const Navbar = () => {
   return (
     <nav>
       <img src={logo} alt="" />
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About Us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li>Contact Us</li>
+      </ul>
     </nav>
   )
 }
