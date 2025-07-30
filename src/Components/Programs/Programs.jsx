0c8c1d74c91181de7218b1ec9f8f74c3b04f83b8
vinnietito <vincentnjuguna69@gmail.com>
2025-07-30 12:19:31 +0300

    Commits

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -13,17 +13,24 @@ const Programs = () => {
       <div className="program">
         <img src={program_1} alt="" />
         <div className="caption">
-            <img src="" alt="" />
+            <img src={program_icon_1} alt="" />
             <p>Graduation Degree</p>
         </div>
       </div>
       <div className="program">
         <img src={program_2} alt="" />
+        <div className="caption">
+            <img src={program_icon_2} alt="" />
+            <p>Master Degree</p>
+        </div>
       </div>
       <div className="program">
         <img src={program_3} alt="" />
+        <div className="caption">
+            <img src={program_icon_3} alt="" />
+            <p>Post Graduation</p>
+        </div>
       </div>
-      
     </div>
   )
 }
