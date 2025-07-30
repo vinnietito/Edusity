049bfc67175b048c8a61b449ceed0a2c28f1bf15
vinnietito <vincentnjuguna69@gmail.com>
2025-07-30 11:46:09 +0300

    Commits

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
@@ -7,7 +8,7 @@ const Hero = () => {
       <div className='hero-text'>
         <h1>We ensure better education for a better world</h1>
         <p>Our cutting-edge curriculum is designed to empower students with knowledge, skils and exprience needed to excel in the dynamic field of education</p>
-        <button className='btn'>Explore more</button>
+        <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
 
       </div>
     </div>
