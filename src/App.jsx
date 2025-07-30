1786b9dd78bf47b8e80c74bbf59b13f4e3417e9e
vinnietito <vincentnjuguna69@gmail.com>
2025-07-30 11:58:11 +0300

    Commits

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,11 @@ const App = () => {
     <div>
       <Navbar/>
       <Hero/>
-      <Programs/>
+      <div className="container">
+        <Programs/>
+        
+      </div>
+      
     </div>
   )
 }
