6bf733870d2a49c4bd1b224eb97039c7bf8ef935
vinnietito <vincentnjuguna69@gmail.com>
2025-07-30 11:23:20 +0300

    Commits

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,12 @@ import './Hero.css'
 const Hero = () => {
   return (
     <div className='hero'>
-      
+      <div className='hero-text'>
+        <h1>We ensure better education for a better world</h1>
+        <p>Our cutting-edge curriculum is designed to empower students with knowledge, skils and exprience needed to excel in the dynamic field of education</p>
+        <button className='btn'>Explore more</button>
+
+      </div>
     </div>
   )
 }
