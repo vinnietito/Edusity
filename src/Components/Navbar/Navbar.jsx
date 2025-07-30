37b4affe8276cab96353802faa4cc84df78ff845
vinnietito <vincentnjuguna69@gmail.com>
2025-07-30 14:01:48 +0300

    Commits

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 
-
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <nav className='container'>
-      <img src={logo} alt="" className='logo' />
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+      <img src={logo} alt="Maple Ridge Academy Logo" className='logo' />
       <ul>
         <li>Home</li>
         <li>Program</li>
