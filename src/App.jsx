2fc8f58602a1c832c87374684fe3cf3ce2bb2946
vinnietito <vincentnjuguna69@gmail.com>
2025-07-30 12:31:54 +0300

    Commits

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
 
 const App = () => {
   return (
@@ -9,8 +10,9 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <div className="container">
+        <Title/>
         <Programs/>
-        
+
       </div>
       
     </div>
