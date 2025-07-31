a4dec2291e576df27691837474c4cf24cd6be5d6
vinnietito <vincentnjuguna69@gmail.com>
2025-07-31 16:45:18 +0300

    Commits

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,6 +4,7 @@ import msg_icon from '../../assets/msg-icon.png'
 import mail_icon from '../../assets/mail-icon.png'
 import phone_icon from '../../assets/phone-icon.png'
 import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
 
 const Contact = () => {
   return (
@@ -41,7 +42,7 @@ const Contact = () => {
             <input type="tel" name='phone' placeholder='Enter your mobile number' />
             <label>Write your message here</label>
             <textarea name="message" rows="6" placeholder='Enter your message'></textarea>
-            <button type='submit' className='btn dark-btn'>Submit now</button>
+            <button type='submit' className='btn dark-btn'>Submit now <img src={white_arrow} alt="" /></button>
         </form>
         {/* Add your form or map/image here */}
       </div>
