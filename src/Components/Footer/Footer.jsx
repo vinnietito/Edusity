f2723f5718bedc861c072c9e9730580a99f6c909
vinnietito <vincentnjuguna69@gmail.com>
2025-08-06 14:37:06 +0300

    Added footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>@ 2025 Maple Ridge Academy. All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
