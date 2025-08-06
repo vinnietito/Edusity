ef22f62a3211357330e603b5d641d600250d884b
vinnietito <vincentnjuguna69@gmail.com>
2025-08-06 15:16:46 +0300

    Added the resct scroll functionality

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false)
@@ -22,12 +23,12 @@ const Navbar = () => {
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt="Maple Ridge Academy Logo" className='logo' />
       <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <li><button className='btn'>Contact Us</button></li>
+        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+        <li><Link to='program' smooth={true} offset={0} duration={500}>Program</Link></li>
+        <li><Link to='about' smooth={true} offset={0} duration={500}>About Us</Link></li>
+        <li><Link to='campus' smooth={true} offset={0} duration={500}>Campus</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={0} duration={500}>Testimonials</Link></li>
+        <li><Link to='contact' smooth={true} offset={0} duration={500} className='btn'>Contact Us</Link></li>
       </ul>
     </nav>
   )
