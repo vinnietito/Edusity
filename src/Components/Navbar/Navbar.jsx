6c93568f5371b0552388b184ec7cd460e357a34c
vinnietito <vincentnjuguna69@gmail.com>
2025-08-06 15:19:36 +0300

    Added the resct scroll functionality

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -24,11 +24,11 @@ const Navbar = () => {
       <img src={logo} alt="Maple Ridge Academy Logo" className='logo' />
       <ul>
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-        <li><Link to='program' smooth={true} offset={0} duration={500}>Program</Link></li>
-        <li><Link to='about' smooth={true} offset={0} duration={500}>About Us</Link></li>
-        <li><Link to='campus' smooth={true} offset={0} duration={500}>Campus</Link></li>
-        <li><Link to='testimonials' smooth={true} offset={0} duration={500}>Testimonials</Link></li>
-        <li><Link to='contact' smooth={true} offset={0} duration={500} className='btn'>Contact Us</Link></li>
+        <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+        <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
+        <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+        <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
       </ul>
     </nav>
   )
